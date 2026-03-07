04411a4a234c674ddc1e11139840ad480b688ccb
srujan <srujanyamali@berkeley.edu>
2026-03-06 20:17:36 -0800

    add a bunch of features

---
+++ .claude/start-dev.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export PATH="/Users/srujanyamali/.nvm/versions/node/v20.19.3/bin:$PATH"
+exec /Users/srujanyamali/.nvm/versions/node/v20.19.3/bin/node /Users/srujanyamali/WebstormProjects/buzzboard/node_modules/.bin/vite --host
